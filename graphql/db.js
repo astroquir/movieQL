073d4986357be316effeb268c261993f1cbef11b
TEAMBRIDGE <teambridge2018n@gmail.com>
2021-11-26 18:13:27 +0900

    #0.13 = #12 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,45 +1,16 @@
-export let movies = [
-    {
-        id : 1,
-        name : "Moon",
-        score : 2
-    },
-    {
-        id : 2,
-        name : "Sun",
-        score : 4        
-    },
-    {
-        id : 3,
-        name : "Chan",
-        score : 5        
-    },
-]
+import fetch from "node-fetch"
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filtered = movies.filter(movie => movie.id === id);
-    return filtered[0];
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
+export const getMovies = (limit, rating) => {
+    let reqURL = API_URL;
+    if (limit > 0) {
+        reqURL += `limit=${limit}`;
     }
-    else {
-        return false;
+    if (rating > 0) {
+        reqURL += `&minimum_rating=${rating}`;
     }
-}
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id : movies.length+1,
-        name,
-        score
-    }
-    movies.push(newMovie);
-    return newMovie;
+    return fetch(reqURL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 }
